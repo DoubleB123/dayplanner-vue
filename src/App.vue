0ec5f47bc08140a029642d5031741233b8dd2425
DoubleB123 <bbrenner321@gmail.com>
2018-06-15 09:25:50 -0700

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <ul class="day-list">
+      <li v-for="dayObj in days" v-bind:key="dayObj.key">
+        <Day v-bind:dayTitle="dayObj.day"></Day>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Day from './components/Day.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      days: [{day: 'Sun', key: 0},
+              {day: 'Mon', key:1},
+              {day: 'Tue', key:2},
+              {day: 'Wed', key:3},
+              {day: 'Thu', key:4},
+              {day: 'Fri', key:5},
+              {day: 'Sat', key: 6}]
+    }
+  },
+  components: {
+    Day
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.day-list {
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding: 0;
+}
+.day-list li {
+  padding: .5em;
+}
+</style>
---
+++ src/components/Day.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class='day-header'>
+    <h1>{{ dayTitle }}</h1>
+    <button v-on:click="addPlan">+</button>
+    <modal v-bind:name="modalName">
+      <h2>Add Plan!</h2>   
+    </modal> 
+  </div>
+</template>
+
+<script>
+  export default {
+      props: ['dayTitle'],
+      data() {
+        return {
+          plans: [],
+        }
+      },
+      methods: {
+        addPlan() {
+          //this.showModal = true;
+          console.log('add');
+          this.$modal.show(this.modalName);
+        }
+      },
+      computed: {
+          modalName(){ 
+            return 'plan-modal-' + this.dayTitle;
+          }
+      }
+  }
+</script>
+
+<style scoped>
+.day-header {
+  display: flex;
+  align-items: center;
+}
+</style>
